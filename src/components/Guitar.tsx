7dfd41c25503a2f07f7b6606655749a38936292d
Miguel Angel Sanchez Papalotzi <msanchezpapalot@gmail.com>
2024-05-04 21:37:28 -0600

    agregando un archivo types

--- src/components/Guitar.tsx
+++ src/components/Guitar.tsx
@@ -1,11 +1,5 @@
+import type {Guitar} from '../types'
 
-type Guitar = {
-    id: number
-    name: string
-    image: string
-    description: string 
-    price: number
-}
 type GuitarProps = {
     guitar: Guitar
     addToCart: (item: Guitar) => void
